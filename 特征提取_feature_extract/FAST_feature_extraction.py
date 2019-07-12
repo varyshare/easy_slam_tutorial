f8eb759fad80a5e87a942e364af480403d417810
varyshare <varyshare@foxmail.com>
2019-07-12 21:22:05 +0800

    实现ORB提取关键点

---
+++ 特征提取_feature_extract/FAST_feature_extraction.py
@@ -0,0 +1,4 @@
+'''
+Features from Accelerated Segment Test (FAST)
+最原始最简单的FAST特征点提取方法Python实现（无金字塔采样）
+'''
---
+++ 特征提取_feature_extract/ORB_feature_extract.py
@@ -0,0 +1,19 @@
+'''
+使用OpenCV实现ORB特征点提取
+作者：知乎@Ai酱
+'''
+# -*- coding: utf-8 -*-
+import cv2
+import numpy as np
+# 0. 读取图片
+img = cv2.imread('./right.png',cv2.IMREAD_GRAYSCALE)
+# 1. 创建一个ORB检测器实例
+orb = cv2.ORB_create()
+# 2. 检测关键点
+keypoint, descript = orb.detectAndCompute(img,None)
+# 3. 绘制关键点
+result_img = cv2.drawKeypoints(img,keypoint,None,color=(0,255,0),flags=cv2.IMREAD_GRAYSCALE)
+# 4. 显示含有关键点的图片
+cv2.imshow("ORB feature point extract",result_img)
+cv2.waitKey(0)
+
